42d6faaca95556ec4ee4da78193181a5790017c6
Kafva <kafva.one@gmail.com>
2025-02-04 07:37:30 +0100

    tests

--- src/tests.zig
+++ src/tests.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Flag = @import("flags.zig").Flag;
+const FlagError = @import("flags.zig").FlagError;
 const FlagIterator = @import("flags.zig").FlagIterator;
 
 const opt_a = "alpha";
@@ -20,4 +21,15 @@ test "Parse ok flags" {
     try std.testing.expectEqualStrings("alpha_argument", opts[0].value.str.?);
     try std.testing.expectEqualStrings("beta_argument", opts[1].value.str.?);
     try std.testing.expect(opts[2].value.active);
+}
+
+test "Parse bad flags" {
+    const cmdline = "./tester --bad";
+    const iter = std.mem.splitScalar(u8, cmdline, ' ');
+    var flags = FlagIterator(std.mem.SplitIterator(u8, .scalar)){ .iter = iter };
+
+    _ = flags.parse(&opts) catch |e| {
+        // TODO is this skipped on success?
+        try std.testing.expectEqual(FlagError.UnexpectedArgument, e);
+    };
 }
