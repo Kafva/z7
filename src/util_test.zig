83e3bfae69c6d549984fac8202a440da325569cf
Kafva <kafva.one@gmail.com>
2025-03-08 09:06:49 +0100

    Bump to zig 0.14.0

--- src/util_test.zig
+++ src/util_test.zig
@@ -77,7 +77,7 @@ fn read_random(
     tmp: *std.testing.TmpDir,
     count: usize
 ) !std.fs.File {
-    var prng = std.rand.DefaultPrng.init(blk: {
+    var prng = std.Random.DefaultPrng.init(blk: {
         var seed: u64 = undefined;
         try std.posix.getrandom(std.mem.asBytes(&seed));
         break :blk seed;
