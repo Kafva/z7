2e7163c204055534b1f91db4bafeabec33869228
Kafva <kafva.one@gmail.com>
2025-03-17 06:56:20 +0100

    wip max depth tree

--- src/huffman.zig
+++ src/huffman.zig
@@ -6,6 +6,8 @@ const HuffmanError = error {
     UnexpectedCharacter,
     UnexpectedEncodedSymbol,
     BadTreeStructure,
+    InternalError,
+    MaxDepthInsufficent,
 };
 
 const NodeEncoding = struct {
@@ -37,12 +39,36 @@ pub const Node = struct {
     /// Only leaf nodes contain a character
     char: ?u8,
     weight: usize,
+    /// Level in the tree, root node is at level 0
+    level: u4,
     left_child_index: ?usize,
     right_child_index: ?usize,
 
-    /// Descending sort comparison method
-    pub fn desc(_:void, lhs: @This(), rhs: @This()) bool {
-        return lhs.weight > rhs.weight;
+    /// Priority sort comparison method:
+    ///
+    /// First: Sort based on level (ascending, lowest first)
+    /// Second: Sort based on weight (descending, highest first)
+    /// Example:
+    ///
+    /// [
+    ///     { .level = 0, .weight = 3 },
+    ///     { .level = 0, .weight = 2 },
+    ///     { .level = 0, .weight = 1 },
+    ///     { .level = 1, .weight = 3 },
+    ///     { .level = 1, .weight = 2 },
+    ///     { .level = 1, .weight = 1 },
+    /// ]
+    ///
+    /// When constructing the tree we want to pop items from the tail of the queue.
+    /// We exhaust the highest remaning level with the lowest weights first.
+    pub fn prio(_:void, lhs: @This(), rhs: @This()) bool {
+        // Returns true if the item should be placed closer to the start of the array
+        if (lhs.level == rhs.level) {
+            return lhs.weight > rhs.weight;
+        }
+        else {
+            return lhs.level < rhs.level;
+        }
     }
 
     pub fn format(
@@ -57,21 +83,38 @@ pub const Node = struct {
 
         if (self.char) |char| {
             if (std.ascii.isPrint(char) and char != '\n') {
-                return writer.print("{{ .weight = {d}, .char = '{c}' }}",
-                                  .{self.weight, char});
+                return writer.print("{{ .level = {d}, .weight = {d}, .char = '{c}' }}",
+                                  .{self.level, self.weight, char});
             } else {
-                return writer.print("{{ .weight = {d}, .char = 0x{x} }}",
-                                  .{self.weight, char});
+                return writer.print("{{ .level = {d}, .weight = {d}, .char = 0x{x} }}",
+                                  .{self.level, self.weight, char});
             }
         } else {
             return writer.print("{{ .weight = {d} }}", .{self.weight});
         }
     }
 
-    pub fn dump(self: @This(), comptime level: u8, pos: []const u8) void {
+    pub fn dump(self: @This(), comptime level: u4, pos: []const u8) void {
         const prefix = util.repeat('-', level) catch unreachable;
         log.debug(@src(), "`{s}{s}: {any} [{d}]", .{prefix, pos, self, level});
         std.heap.page_allocator.free(prefix);
+    }
+
+    /// Calculate how many child levels are below the current node recursively
+    pub fn levels_below(self: @This(), array: *const []Node) u4 {
+        var child_levels_left = 0;
+        var child_levels_right = 0;
+
+        if (self.left_child_index) |idx| {
+            child_levels_left += array.*[idx].levels_below(array);
+        }
+        if (self.right_child_index) |idx| {
+            child_levels_right += array.*[idx].levels_below(array);
+        }
+
+        // Pick the largest side
+        return if (child_levels_right > child_levels_left) child_levels_right
+               else child_levels_left;
     }
 };
 
@@ -91,10 +134,9 @@ pub const Huffman = struct {
         util.dump_hashmap(usize, frequencies);
 
         // 2. Create a queue of nodes to place into the tree
-        var queue_cnt: usize = frequencies.count();
-        const queue = try allocator.alloc(Node, queue_cnt);
+        const queue_cnt: usize = frequencies.count();
+        var queue = try allocator.alloc(Node, queue_cnt);
 
-        var array_cnt: usize = 0;
         // The array will grow if the capacity turns out to be too low
         var array = try std.ArrayList(Node).initCapacity(allocator, 2*queue_cnt);
 
@@ -104,12 +146,13 @@ pub const Huffman = struct {
             const weight = frequencies.*.get(key.*).?;
             queue[index] = Node {
                 .char = key.*,
+                .level = 15, // placeholder
                 .weight = weight,
                 .left_child_index = undefined,
                 .right_child_index = undefined
             };
             // Sort in descending order with the largest node first
-            std.sort.insertion(Node, queue[0..index], {}, Node.desc);
+            std.sort.insertion(Node, queue[0..index], {}, Node.prio);
             index += 1;
         }
 
@@ -118,29 +161,110 @@ pub const Huffman = struct {
         // 3. Create the tree, we need to make sure that we do not grow
         // the tree deeper than 15 levels so that every leaf can be encoded
         // with a u16.
+        //
+        // Level 0: 2^0 nodes
+        // Level 1: 2^1 nodes
+        // Level 2: 2^2 nodes
+        // ...
+        // Level 15: 2^15 nodes
+        //
+        // Iterative approach, try to create a tree with all slots filled up to level 2
+        // On failure, try again with all slots up to level 3, etc.
+        for (2..15) |i| {
+            construct_max_depth_tree(&queue, queue_cnt, &array, @truncate(i)) catch |e| {
+                if (e == HuffmanError.MaxDepthInsufficent) {
+                    continue;
+                }
+                return e;
+            };
+            break;
+        }
+
+        var enc_map = try allocator.alloc(?NodeEncoding, 256);
+        try build_canonical_encoding(&array, &enc_map);
+
+        return @This(){ .array = array, .enc_map = enc_map };
+    }
+
+    /// Construct a Huffman tree from `queue` into `array` which does not
+    /// exceed the provided `max_level`, returns an error if the provided
+    /// `max_level` is insufficient.
+    fn construct_max_depth_tree(
+        queue: *[]Node,
+        queue_initial_cnt: usize,
+        array: *std.ArrayList(Node),
+        max_level: u4,
+    ) !void {
+        var queue_cnt: usize = queue_initial_cnt;
+        var array_cnt: usize = 0;
+        var filled_cnt: usize = 0;
+        var current_level: u4 = max_level;
+
+        // Let all nodes start at the bottom
+        for (0..queue_initial_cnt) |i| {
+            queue.*[i].level = max_level;
+            std.debug.print("queue: {any}\n", .{queue.*[i]});
+        }
+
+
         while (queue_cnt > 0) {
+            if (current_level == 0) {
+                if (queue_cnt > 1) {
+                    log.debug(
+                        @src(),
+                        "failed to construct {}-depth tree: {} nodes left",
+                        .{max_level, queue_cnt}
+                    );
+                    return HuffmanError.MaxDepthInsufficent;
+                }
+            }
+
             if (queue_cnt == 1) {
-                // The root node will always be the last item
-                try array.append(queue[queue_cnt - 1]);
+                // OK: last node from the priority queue has already been given children,
+                // just save it and quit.
+                try array.*.append(queue.*[0]);
                 queue_cnt -= 1;
                 array_cnt += 1;
                 continue;
             }
 
-            // Pick the two elements from the queue with the lowest weight
-            const left_child = queue[queue_cnt - 2];
-            const right_child = queue[queue_cnt - 1];
+            if (filled_cnt == std.math.pow(usize, 2, current_level)) {
+                // OK: current level has been filled
+                filled_cnt = 0;
+                current_level -= 1;
+                continue;
+            }
+
+            // Current level has not been filled, pick the two nodes with the lowest weight
+            // from the current level
+            const left_child = queue.*[queue_cnt - 2];
+            const right_child = queue.*[queue_cnt - 1];
+
+            // If the nodes are at a higher level than expected we have an internal error
+            if (left_child.level > current_level) {
+                log.err(@src(), "bad level of left node: {}", .{left_child.level});
+                return HuffmanError.InternalError;
+            }
+            if (right_child.level > current_level) {
+                log.err(@src(), "bad level of right node: {}", .{right_child.level});
+                return HuffmanError.InternalError;
+            }
 
             // Save the children from the queue into the backing array
-            try array.append(left_child);
-            try array.append(right_child);
+            // XXX: the backing array is unsorted, the tree structure is derived from each `Node`
+            try array.*.append(left_child);
+            try array.*.append(right_child);
             array_cnt += 2;
+            filled_cnt += 2;
 
             // Create the parent node
-            const parent_weight = left_child.weight + right_child.weight;
+            // We always create parent node with both child positions filled,
+            // the child pointers of this node will not change after creation.
             const parent_node = Node {
                 .char = undefined,
-                .weight = parent_weight,
+                .weight = left_child.weight + right_child.weight,
+                // The parent will be one level higher than the children
+                .level = current_level - 1,
                 .left_child_index = array_cnt - 2,
                 .right_child_index = array_cnt - 1
             };
@@ -148,14 +272,9 @@ pub const Huffman = struct {
             // Insert the parent node into the priority queue, overwriting
             // one of the children and dropping the other
             queue_cnt -= 1;
-            queue[queue_cnt - 1] = parent_node;
-            std.sort.insertion(Node, queue[0..queue_cnt], {}, Node.desc);
+            queue.*[queue_cnt - 1] = parent_node;
+            std.sort.insertion(Node, queue.*[0..queue_cnt], {}, Node.prio);
         }
-
-        var enc_map = try allocator.alloc(?NodeEncoding, 256);
-        try build_canonical_encoding(&array, &enc_map);
-
-        return @This(){ .array = array, .enc_map = enc_map };
     }
 
     /// Convert the Huffman tree stored in `array` into its canonical form, it
@@ -268,14 +387,14 @@ pub const Huffman = struct {
 
             if (self.enc_map[@intCast(c)]) |enc| {
                 if (enc.bit_shift >= 15) {
-                    log.err(@src(), "Unexpected bit shift: {any}", .{enc});
+                    log.err(@src(), "unexpected bit shift: {any}", .{enc});
                     return HuffmanError.BadTreeStructure;
                 }
                 // Write the translation to the output stream
                 try writer.writeBits(enc.bits, enc.bit_shift + 1);
                 written_bits += enc.bit_shift + 1;
             } else {
-                log.err(@src(), "Unexpected byte: 0x{x}", .{c});
+                log.err(@src(), "unexpected byte: 0x{x}", .{c});
                 return HuffmanError.UnexpectedCharacter;
             }
         }
@@ -334,7 +453,7 @@ pub const Huffman = struct {
             if (self.array.items[index].char) |char| {
                 return char;
             } else {
-                log.err(@src(), "Missing character from leaf node", .{});
+                log.err(@src(), "missing character from leaf node", .{});
                 return HuffmanError.BadTreeStructure;
             }
 
@@ -370,7 +489,7 @@ pub const Huffman = struct {
         const right_child_index = array.items[index].right_child_index;
 
         if (bit_shift >= 15) {
-            log.err(@src(), "Huffman tree too deep: node requires {} bits", .{bit_shift});
+            log.err(@src(), "huffman tree too deep: node requires {} bits", .{bit_shift});
             return HuffmanError.BadTreeStructure;
         }
 
@@ -380,7 +499,7 @@ pub const Huffman = struct {
                 const enc = NodeEncoding { .bit_shift = bit_shift, .bits = bits };
                 enc_map.*[char] = enc;
             } else {
-                log.err(@src(), "Missing character from leaf node", .{});
+                log.err(@src(), "missing character from leaf node", .{});
                 return HuffmanError.BadTreeStructure;
             }
         } else {
@@ -408,7 +527,7 @@ pub const Huffman = struct {
     fn dump_tree(self: @This(), comptime level: u4, index: usize) void {
         // Compile time generated strings are built based on the level
         if (level >= 15) {
-            log.err(@src(), "Reached maximum depth: {d}", .{level});
+            log.err(@src(), "reached maximum depth: {d}", .{level});
             return;
         }
 
