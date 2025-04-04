1ca2ab767ad4c52a78a2bb0263997abc323a7560
Kafva <kafva.one@gmail.com>
2025-04-04 07:23:50 +0200

    wip ref check

--- src/flate_test.zig
+++ src/flate_test.zig
@@ -12,7 +12,7 @@ const libflate = @cImport({
 
 const cleanup_tmpdir = false;
 const max_size = 40*1024;
-const ret_type = @typeInfo(@TypeOf(check_z7_flate)).@"fn".return_type.?;
+const ret_type = @typeInfo(@TypeOf(check_z7_ok)).@"fn".return_type.?;
 
 fn run(
     inputfile: []const u8,
@@ -31,7 +31,7 @@ fn run(
 }
 
 /// Verify that z7 can decompress its own output
-fn check_z7_flate(
+fn check_z7_ok(
     allocator: std.mem.Allocator,
     inputfile: []const u8,
     compressed: *std.fs.File,
@@ -67,7 +67,7 @@ fn check_z7_flate(
 }
 
 /// Verify that the Golang flate implementation can decompress z7 output
-fn check_flate_decompress_go(
+fn check_z7_decompress_ref(
     allocator: std.mem.Allocator,
     inputfile: []const u8,
     compressed: *std.fs.File,
@@ -107,8 +107,51 @@ fn check_flate_decompress_go(
     try util.eql(allocator, in, decompressed.*);
 }
 
+/// Verify that z7 can decompress output from the Golang flate implementation
+fn check_z7_compress_ref(
+    allocator: std.mem.Allocator,
+    inputfile: []const u8,
+    compressed: *std.fs.File,
+    decompressed: *std.fs.File,
+) !void {
+    var in: std.fs.File = undefined;
+    var in_size: usize = undefined;
+
+    var tmp = std.testing.tmpDir(.{});
+    if (cleanup_tmpdir) {
+        defer tmp.cleanup();
+    }
+
+    try util.setup(
+        allocator,
+        &tmp,
+        inputfile,
+        &in,
+        &in_size,
+        compressed,
+        decompressed
+    );
+    defer in.close();
+
+    const inputfile_s = libflate.GoString{
+        .p = inputfile.ptr,
+        .n = @intCast(inputfile.len)
+    };
+
+    const compressed_path_s = try util.go_str_tmp_filepath(allocator, &tmp, "compressed.bin");
+    const compressed_len = libflate.FlateCompress(inputfile_s, compressed_path_s);
+    try std.testing.expect(compressed_len > 0);
+
+    try util.log_result("go", inputfile, in_size, @intCast(compressed_len));
+
+    try Decompress.decompress(allocator, compressed.*, decompressed.*, 0);
+
+    // Verify correct decompression
+    try util.eql(allocator, in, decompressed.*);
+}
+
 /// Verify that the Golang implementation is ok for ffi
-fn check_flate_reference_ok(
+fn check_ref_ok(
     allocator: std.mem.Allocator,
     inputfile: []const u8,
     compressed: *std.fs.File,
@@ -152,32 +195,33 @@ fn check_flate_reference_ok(
     try util.eql(allocator, in, decompressed.*);
 }
 
-test "Flate on empty file" {
-    try run("tests/testdata/empty", check_z7_flate);
-    // try run("tests/testdata/empty", check_flate_reference_ok);
-}
+// test "Flate on empty file" {
+//     try run("tests/testdata/empty", check_z7_ok);
+//     // try run("tests/testdata/empty", check_ref_ok);
+// }
 
-test "Flate on simple text" {
-    try run("tests/testdata/flate_test.txt", check_z7_flate);
-    try run("tests/testdata/flate_test.txt", check_flate_reference_ok);
-}
+// test "Flate on simple text" {
+//     try run("tests/testdata/flate_test.txt", check_z7_ok);
+//     try run("tests/testdata/flate_test.txt", check_ref_ok);
+// }
 
 test "Flate on short simple text" {
-    try run("tests/testdata/simple.txt", check_z7_flate);
-    try run("tests/testdata/simple.txt", check_flate_reference_ok);
-    //try run("tests/testdata/simple.txt", check_flate_decompress_go);
+    //try run("tests/testdata/simple.txt", check_z7_ok);
+    //try run("tests/testdata/simple.txt", check_ref_ok);
+    try run("tests/testdata/simple.txt", check_z7_decompress_ref);
+    //try run("tests/testdata/simple.txt", check_z7_compress_ref);
 }
 
-test "Flate on 9001 repeated characters" {
-    try run("tests/testdata/over_9000_a.txt", check_z7_flate);
-    try run("tests/testdata/over_9000_a.txt", check_flate_reference_ok);
-}
+// test "Flate on 9001 repeated characters" {
+//     try run("tests/testdata/over_9000_a.txt", check_z7_ok);
+//     try run("tests/testdata/over_9000_a.txt", check_ref_ok);
+// }
 
-test "Flate on rfc1951.txt" {
-    try run("tests/testdata/rfc1951.txt", check_z7_flate);
-    try run("tests/testdata/rfc1951.txt", check_flate_reference_ok);
-}
+// test "Flate on rfc1951.txt" {
+//     try run("tests/testdata/rfc1951.txt", check_z7_ok);
+//     try run("tests/testdata/rfc1951.txt", check_ref_ok);
+// }
 
-test "Flate on random data" {
-    try run(util.random_label, check_z7_flate);
-}
+// test "Flate on random data" {
+//     try run(util.random_label, check_z7_ok);
+// }
